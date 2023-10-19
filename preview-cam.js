07676044cd278d56ef18088276b5a1a4580e2d75
Dustin Freeman <dustin.freeman@gmail.com>
2023-10-18 20:40:22 -0400

    WIP

--- preview-cam.js
+++ preview-cam.js
@@ -1,17 +1,32 @@
 var video = document.querySelector("#preview");
 
-var videoRequest = true;
+function startStreamingVideo(videoRequest = true) {
+    if (navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices.getUserMedia({ video: videoRequest })
+            .then(function (stream) {
+                video.srcObject = stream;
+            })
+            .catch(function (err0r) {
+                console.log("Something went wrong!", err0r);
+            });
+    }
+}
 
 if (typeof forcePreviewCamera !== 'undefined') {
     console.log("forcePreviewCamera: ", forcePreviewCamera);
-}
 
-if (navigator.mediaDevices.getUserMedia) {
-    navigator.mediaDevices.getUserMedia({ video: true })
-        .then(function (stream) {
-            video.srcObject = stream;
-        })
-        .catch(function (err0r) {
-            console.log("Something went wrong!", err0r);
-        });
+    navigator.mediaDevices.enumerateDevices().then((value) => {
+        var webcams = value.filter(device => device.kind === 'videoinput');
+        for (const [index, w] of webcams.entries()) {
+            console.log(w);
+            if (w.label === forcePreviewCamera) {
+                startStreamingVideo({
+                    deviceId: { exact: w.deviceId }
+                })
+            }
+        };
+    })
+
+} else {
+    startStreamingVideo();
 }
